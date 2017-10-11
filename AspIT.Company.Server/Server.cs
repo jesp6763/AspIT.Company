df0abea51714a49af9a58555032acd20cbe201a0
Jesper K <jesp6763@edu.campusvejle.dk>
2017-10-11 14:02:25 +0200

    Added summaries

--- AspIT.Company.Server/Server.cs
+++ AspIT.Company.Server/Server.cs
@@ -9,13 +9,32 @@ namespace AspIT.Company.Server
 {
     public class Server
     {
+        /// <summary>
+        /// Gets or sets the display name of the server
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Gets the TcpListener
+        /// </summary>
         public TcpListener TcpListener { get; }
 
-        public Server(string name, TcpListener tcpListener)
+        /// <summary>
+        /// Initializes a new instance of the Server class
+        /// </summary>
+        /// <param name="tcpListener">A TcpListener</param>
+        public Server(TcpListener tcpListener)
+        {
+            TcpListener = tcpListener;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Server class
+        /// </summary>
+        /// <param name="name">The name of the server. This is only for display</param>
+        /// <param name="tcpListener">A TcpListener</param>
+        public Server(string name, TcpListener tcpListener) : this(tcpListener)
         {
             Name = name;
-            TcpListener = tcpListener;
         }
     }
 }
