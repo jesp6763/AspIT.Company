171e2c0e681db9cf181b1852209ad314a251f5a5
Jesper K <jesp6763@edu.campusvejle.dk>
2017-10-11 13:54:05 +0200

    Created simple client window

---
+++ AspIT.Company.Clients.WindowsDesktopGui/ClientWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AspIT.Company.Clients.Communications;
+
+namespace AspIT.Company.Clients.WindowsDesktop.Gui
+{
+    /// <summary>
+    /// Interaction logic for ClientWindow.xaml
+    /// </summary>
+    public partial class ClientWindow : Window
+    {
+        public ClientWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            LoginHandler.Logout();
+        }
+    }
+}
