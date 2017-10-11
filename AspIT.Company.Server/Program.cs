493dfc0d28773342a501421594ca3f90d0cb3bc8
Jesper K <jesp6763@edu.campusvejle.dk>
2017-10-11 13:53:39 +0200

    Created server and added basic functionality

---
+++ AspIT.Company.Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspIT.Company.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server("Test Server", new TcpListener(IPAddress.Parse("127.0.0.1"), 27013));
+            server.TcpListener.Start();
+            PrintServerInformation(server);
+            Console.ReadKey();
+        }
+
+        private static void PrintServerInformation(Server server)
+        {
+            Console.WriteLine($"Server ip: {server.TcpListener.LocalEndpoint}");
+        }
+    }
+}
---
+++ AspIT.Company.Server/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspIT.Company.Server
+{
+    public class Server
+    {
+        public string Name { get; set; }
+        public TcpListener TcpListener { get; }
+
+        public Server(string name, TcpListener tcpListener)
+        {
+            Name = name;
+            TcpListener = tcpListener;
+        }
+    }
+}
