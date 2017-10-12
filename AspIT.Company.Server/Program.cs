4b5c11cffdd4e5a019b770357c2cf4df9067c714
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-10-12 13:15:57 +0200

    Pasted test code in

--- AspIT.Company.Server/Program.cs
+++ AspIT.Company.Server/Program.cs
@@ -14,7 +14,51 @@ namespace AspIT.Company.Server
         {
             Server server = new Server("Test Server", IPAddress.Parse("127.0.0.1"), 27013, true);
             PrintServerInformation(server);
-            Console.ReadKey();
+
+            // Buffer for reading data
+            byte[] bytes = new byte[1024];
+            string data;
+
+            //Enter the listening loop
+            while(true)
+            {
+                Console.WriteLine("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also user server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine($"Client from address {client.Client.RemoteEndPoint} connected");
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                i = stream.Read(bytes, 0, bytes.Length);
+
+                while(i != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine(String.Format("Received: {0}", data));
+
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine(String.Format("Sent: {0}", data));
+
+                    i = stream.Read(bytes, 0, bytes.Length);
+
+                }
+
+                // Shutdown and end connection
+                client.Close();
+            }
         }
 
         private static void PrintServerInformation(Server server)
