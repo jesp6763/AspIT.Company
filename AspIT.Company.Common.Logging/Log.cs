8e337570b3a203012cf167f623b6860ba4f92fe0
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-10-13 10:10:18 +0200

    Made simple logging functionality

---
+++ AspIT.Company.Common.Logging/Log.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace AspIT.Company.Common.Logging
+{
+    public class Log
+    {
+        public struct LogData
+        {
+            public LogData(string message)
+            {
+                DateTime = DateTime.Now;
+                Message = message;
+            }
+
+            public LogData(DateTime dateTime, string message)
+            {
+                DateTime = dateTime;
+                Message = message;
+            }
+
+            /// <summary>
+            /// Gets the date and time the log was made
+            /// </summary>
+            public DateTime DateTime { get; }
+            /// <summary>
+            /// Gets the log message
+            /// </summary>
+            public string Message { get; }
+        }
+
+        private static List<LogData> logs;
+
+        /// <summary>
+        /// Generates a log file
+        /// </summary>
+        public static void Create()
+        {
+            string path = "Logs";
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach(LogData log in logs)
+            {
+                stringBuilder.AppendLine($"{log.DateTime.ToShortDateString()} {log.DateTime.ToShortTimeString()}: {log.Message}");
+            }
+
+            if(Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            File.AppendAllText(Path.Combine(path, $"{DateTime.Now.ToString()} logs.txt"), stringBuilder.ToString());
+        }
+
+        public static void AddLog(LogData logData)
+        {
+            logs.Add(logData);
+        }
+    }
+}
