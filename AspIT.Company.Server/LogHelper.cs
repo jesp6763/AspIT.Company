8edce983fa40c2e6e447beb567124e8afb4639da
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-10-13 11:53:39 +0200

    Created LogHelper

---
+++ AspIT.Company.Server/LogHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspIT.Company.Common.Logging;
+
+namespace AspIT.Company.Server
+{
+    public static class LogHelper
+    {
+        public static void AddLog(string message)
+        {
+            Console.WriteLine(message);
+            Log.AddLog(new Log.LogData(message));
+        }
+    }
+}
