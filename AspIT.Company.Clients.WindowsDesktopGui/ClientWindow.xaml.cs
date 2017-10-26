ac557923129c856f1a388063ef8d4a6409139dde
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-10-26 13:22:19 +0200

    Added send user object to on Send button click

--- AspIT.Company.Clients.WindowsDesktopGui/ClientWindow.xaml.cs
+++ AspIT.Company.Clients.WindowsDesktopGui/ClientWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using AspIT.Company.Clients.Communications;
 
 namespace AspIT.Company.Clients.WindowsDesktop.Gui
@@ -34,6 +36,13 @@ namespace AspIT.Company.Clients.WindowsDesktop.Gui
         private void LogoutBtn_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void SendUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            NetworkStream stream = ConnectionHandler.CurrentConnection.GetStream();
+            formatter.Serialize(stream, LoginHandler.CurrentUser);
         }
     }
 }
