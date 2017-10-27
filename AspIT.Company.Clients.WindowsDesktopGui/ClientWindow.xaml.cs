5f894a330ed5b65152133953af090815a2e60368
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-10-27 11:56:58 +0200

    Added stuff to be able to send images to the server

--- AspIT.Company.Clients.WindowsDesktopGui/ClientWindow.xaml.cs
+++ AspIT.Company.Clients.WindowsDesktopGui/ClientWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+using System.IO;
 using AspIT.Company.Clients.Communications;
 
 namespace AspIT.Company.Clients.WindowsDesktop.Gui
@@ -30,7 +32,7 @@ namespace AspIT.Company.Clients.WindowsDesktop.Gui
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             LoginHandler.Logout();
-            Application.Current.MainWindow.Show();
+            System.Windows.Application.Current.MainWindow.Show();
         }
 
         private void LogoutBtn_Click(object sender, RoutedEventArgs e)
@@ -40,9 +42,22 @@ namespace AspIT.Company.Clients.WindowsDesktop.Gui
 
         private void SendUserBtn_Click(object sender, RoutedEventArgs e)
         {
-            BinaryFormatter formatter = new BinaryFormatter();
-            NetworkStream stream = ConnectionHandler.CurrentConnection.GetStream();
-            formatter.Serialize(stream, LoginHandler.CurrentUser);
+            if(imagePath.Text != string.Empty)
+            {
+                // Get image file bytes
+                byte[] imageBytes = File.ReadAllBytes(imagePath.Text);
+
+                // Send bytes to server
+                ConnectionHandler.CurrentConnection.Client.Send(imageBytes);
+                System.Windows.MessageBox.Show("Image has been sent");
+            }
+        }
+
+        private void ImageBrowseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            imagePath.Text = openFileDialog.FileName;
         }
     }
 }
